c95b17b96f2d23ca624b1ce73519986fc9a73bc2
JScharm <student@bluc02mg0k0fh51.local>
2016-04-04 10:36:09 -0500

    moments

---
+++ Bro Hockey/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Bro Hockey
+//
+//  Created by JScharm on 4/4/16.
+//  Copyright © 2016 JScharm. All rights reserved.
+//  josh version
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
